31dd0638f3479cb895aaca1233d06a57889eb646
Yi-Fan Tsai <yifan.tsai@gmail.com>
2017-02-15 22:08:42 -0800

    change tabs to white spaces

--- contest_volumes/volume_100/10041.cpp
+++ contest_volumes/volume_100/10041.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main()
 {
-	int cases;
-	int r, s[500], dis, i;
+    int cases;
+    int r, s[500], dis, i;
 
-	for (cin >> cases; cases>0; cases--){
-		cin >> r;
-		for (i=0; i<r; i++)
-			cin >> s[i];
-		sort(s, s+r);
-		for (i=0, r--, dis=0; i<r; i++, r--)
-			dis+=s[r]-s[i];
-		cout << dis << endl;
-	}
-	
-	return 0;
+    for (cin >> cases; cases>0; cases--){
+        cin >> r;
+        for (i=0; i<r; i++)
+            cin >> s[i];
+        sort(s, s+r);
+        for (i=0, r--, dis=0; i<r; i++, r--)
+            dis+=s[r]-s[i];
+        cout << dis << endl;
+    }
+
+    return 0;
 }
